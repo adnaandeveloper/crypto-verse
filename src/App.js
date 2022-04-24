4b575801ea4b7af5af3e0d7cada8a96c07533040
amaydeveloper <amaydeveloper@MacBook-Pro-tilhrende-amaydeveloper.local>
2022-04-24 16:05:38 +0200

    implemented navbar & footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+import {
+  Navbar,
+  HomePage,
+  Exchanges,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from './components';
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <h1> Crypto Verse</h1>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:CoinId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: 'white', textAlign: 'center' }}
+          >
+            Cryptoverse <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/"> Home</Link>
+            <Link to="/exchanges"> Exchanges</Link>
+            <Link to="/news"> News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
 };
